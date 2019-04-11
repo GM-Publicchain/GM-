64a100b3c5f857478534d74d73c2316d7af115aa
GM（敢吗公链） <46987951+GM-Publicchain@users.noreply.github.com>
2019-04-11 13:25:35 +0800

    Update init.go

--- plugin/init.go
+++ plugin/init.go
@@ -1,8 +1,8 @@
 package plugin
 
 import (
-	_ "github.com/bityuan/bityuan/plugin/consensus/init" //register consensus init package
-	_ "github.com/bityuan/bityuan/plugin/crypto/init"
-	_ "github.com/bityuan/bityuan/plugin/dapp/init"
-	_ "github.com/bityuan/bityuan/plugin/store/init"
+	_ "github.com/ganma/GM/plugin/consensus/init" //register consensus init package
+	_ "github.com/ganma/GM/plugin/crypto/init"
+	_ "github.com/ganma/GM/plugin/dapp/init"
+	_ "github.com/ganma.n/plugin/store/init"
 )
